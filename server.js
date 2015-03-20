ea315103f36a492e0f6828d73ff3091384e1ae47
Mary Rose Cook <mary@maryrosecook.com>
2015-03-20 13:16:24 -0400

    make db write to disk

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var http = require("http");
 var querystring = require("querystring");
 var url = require("url");
+var fs = require("fs");
 
 var routes = {
   "/get": function(request, response, db) {
     var query = urlToQueryObj(request.url);
-    sendResponse(response, 200, "text/html", db[query.key]);
+    sendResponse(response, 200, "text/html", db.get(query.key));
   },
 
   "/set": function(request, response, db) {
     var query = urlToQueryObj(request.url);
     var key = Object.keys(query)[0]; // set one pair if there are many
 
-    db[key] = query[key];
+    db.set(key, query[key]);
     sendResponse(response, 200, "text/html", "Set");
   }
 };
@@ -26,10 +27,51 @@ function sendResponse(response, httpCode, contentType, body) {
   return response.end(body);
 };
 
-function startServer() {
-  var db = {};
+function Db(filePath) {
+  var isSaving = false;
+  var isSaveQueued = false;
 
-  return http.createServer(function (request, response) {
+  var data = {};
+  if (fs.existsSync(filePath)) {
+    var jsonStr = fs.readFileSync(filePath, "utf8");
+    if (jsonStr.length > 0) {
+      data = JSON.parse(jsonStr);
+    }
+  }
+
+  this.set = function(key, value) {
+    data[key] = value;
+    saveDb();
+  };
+
+  this.get = function(key) {
+    return data[key];
+  };
+
+  this.isSaving = function() {
+    return isSaving;
+  };
+
+  function saveDb() {
+    if (!isSaving) {
+      isSaving = true;
+      fs.writeFile(filePath, JSON.stringify(data), function() {
+        isSaving = false;
+        if (isSaveQueued) {
+          isSaveQueued = false;
+          saveDb();
+        }
+      });
+    } else {
+      isSaveQueued = true;
+    }
+  }
+};
+
+function startServer(la) {
+  var db = new Db("data.json");
+
+  var server = http.createServer(function (request, response) {
     var handler = routes[url.parse(request.url).pathname];
 
     if (handler !== undefined) {
@@ -38,6 +80,18 @@ function startServer() {
       return sendResponse(response, 404, "text/html", "Not found");
     }
   }).listen(4000);
+
+  server.shutDown = function(cb) {
+    if (!db.isSaving()) {
+      server.close(cb);
+    } else {
+      setTimeout(function() {
+        server.shutDown(cb);
+      }, 10);
+    }
+  };
+
+  return server;
 };
 
 module.exports = startServer;
--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,5 +1,6 @@
 var startServer = require("../server.js");
 var http = require("http");
+var fs = require("fs");
 
 function getRequest(url, cb) {
   http.get(url, function(response) {
@@ -15,14 +16,30 @@ function getRequest(url, cb) {
   })
 };
 
+function all(ops, cb) {
+  var completedOps = 0;
+  function complete(i) {
+    completedOps++;
+    if (completedOps === ops.length - 1) {
+      cb();
+    }
+  };
+
+  ops.forEach(function(op) { op(complete); })
+};
+
 describe("server", function() {
   var server;
   beforeEach(function() {
+    if (fs.existsSync("data.json")) {
+      fs.unlinkSync("data.json");
+    }
+
     server = startServer();
   });
 
   afterEach(function() {
-    server.close();
+    server.shutDown();
   });
 
   it("should return 404 for root", function(done) {
@@ -69,6 +86,45 @@ describe("server", function() {
             expect(body).toEqual("160");
             done();
           });
+        });
+      });
+    });
+  });
+
+  it("should load existing db", function(done) {
+    getRequest("http://localhost:4000/set?name=mary", function(_, body) {
+      server.shutDown(function() {
+        server = startServer();
+        getRequest("http://localhost:4000/get?key=name", function(_, body) {
+          expect(body).toEqual("mary");
+          done();
+        });
+      });
+    });
+  });
+
+  it("should save a bunch of pairs", function(done) {
+    var setOps = [];
+    for (var i = 1; i <= 1000; i++) {
+      setOps.push(i);
+    }
+
+    all(setOps.map(function(i) {
+      return function(complete) {
+        getRequest("http://localhost:4000/set?" + i + "=" + i, complete);
+      }
+    }), function() {
+      server.shutDown(function() {
+        server = startServer();
+        all(setOps.map(function(i) {
+          return function(complete) {
+            getRequest("http://localhost:4000/get?key=" + i, function(_, value) {
+              expect(value).toEqual(i.toString());
+              complete();
+            });
+          }
+        }), function() {
+          done()
         });
       });
     });
